8e728a1780437a315d3b1b0e2416d76af5057a09
MaddieFish <MaddieFish@users.noreply.github.com>
2018-09-16 08:14:55 -0400

    Sketch 4 (Mouse Movements)
    Finished, but there is an error message that I cannot figure out at the moment, that crashes the sketch. JS and html.

---
+++ Sketch 4/sketch4.js
@@ -0,0 +1,40 @@
+//Sketch 4
+
+function setup(){
+	createCanvas(800, 800);
+}
+
+function draw(){
+	background(100, 30, 80);
+	translate(width/2, height/2);
+
+	for(var n = 0; n < 30; n++){
+
+		if(mouseIsPressed){
+			rotate(3+(PI/sqrt(mouseX)));
+			fill(n*20, 100, mouseY);
+
+		}else{
+			rotate(10+(PI/sqrt(mouseY)));
+			fill(mouseX, 100, n*10);
+
+		}
+
+		design();
+	}
+
+}
+
+function design(){
+	strokeWeight(5);
+	line(30, 20, mouseX, mouseY);
+	for(var k = 0; k< 5; k++){
+		noStroke();
+		rect(mouseX, 0, 50, 10 + mouseY, k*10);
+		rect(mouseX, 50, k*10, 10 + mouseY, k*10);
+	}
+}
+
+//ERROR MESSAGE
+//p5.js:51485 Uncaught DOMException: Failed to execute 'arcTo' on 'CanvasRenderingContext2D': The radius provided (-4) is negative.
+//at p5.Renderer2D.rect 
